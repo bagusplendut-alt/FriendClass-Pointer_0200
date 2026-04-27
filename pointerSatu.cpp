01fd9883df7f5519dc2195cacaaea3cb49b5a13a
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-27 15:29:03 +0700

    memberi int main dalam bagian mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,18 @@ class mahasiswa{
     cout << "No Induk = " << nim << endl;
   }
 };
+
+int main(){
+  mahasiswa mhs{1};
+  mhs.shownim();
+
+  mahasiswa &refmhs = mhs;
+  refmhs.nim = 2;
+  mhs.shownim();
+
+  
+  mahasiswa *pmhs = &mhs;
+  pmhs->nim = 3;
+  pmhs->shownim();
+  return 0;
+}
